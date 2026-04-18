64e0ee5cbb3e46ff2dbdc337454eac79e8571a0f
ksong008 <ksong008@gmail.com>
2026-04-18 19:55:48 +0800

    Fix unit test assumptions for daecore

--- common/netutils/ip46_test.go
+++ common/netutils/ip46_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 func TestResolveIp46(t *testing.T) {
+	direct.InitDirectDialers("223.5.5.5:53")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	ip46, err4, err6 := ResolveIp46(ctx, direct.SymmetricDirect, netip.MustParseAddrPort("223.5.5.5:53"), "ipv6.google.com", "udp", false)
